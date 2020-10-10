40597d2d1bf43ccb8e8447a78e5e64b15cf7af96
Jan Hovland <jan.hovland@lyse.net>
2020-10-10 18:32:48 +0200

    Update

---
+++ SendMail/MailView.swift
@@ -0,0 +1,63 @@
+//
+//  MailView.swift
+//  SendMail
+//
+//  Created by Jan Hovland on 10/10/2020.
+//
+
+import SwiftUI
+import MessageUI 
+
+struct MailView: UIViewControllerRepresentable {
+    
+    @Binding var isShowing: Bool
+    @Binding var toRecipients: [String]
+    @Binding var subject: String
+    @Binding var messageBody: String
+    @Binding var result: Result<MFMailComposeResult, Error>?
+
+    class Coordinator: NSObject, MFMailComposeViewControllerDelegate {
+        
+        @Binding var isShowing: Bool
+        @Binding var result: Result<MFMailComposeResult, Error>?
+        
+        init(isShowing: Binding<Bool>,
+             result: Binding<Result<MFMailComposeResult, Error>?>) {
+            _isShowing = isShowing
+            _result = result
+        }
+        
+        func mailComposeController(_ controller: MFMailComposeViewController,
+                                   didFinishWith result: MFMailComposeResult,
+                                   error: Error?) {
+            defer {
+                isShowing = false
+            }
+            guard error == nil else {
+                self.result = .failure(error!)
+                return
+            }
+            self.result = .success(result)
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(isShowing: $isShowing,
+                           result: $result)
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<MailView>) -> MFMailComposeViewController {
+        let vc = MFMailComposeViewController()
+        vc.setToRecipients(toRecipients)
+        vc.setSubject(subject)
+        vc.setMessageBody(messageBody, isHTML: true)
+        vc.mailComposeDelegate = context.coordinator
+        return vc
+    }
+    
+    func updateUIViewController(_ uiViewController: MFMailComposeViewController,
+                                context: UIViewControllerRepresentableContext<MailView>) {
+        
+    }
+}
+
---
+++ SendMail/SendMailView.swift
@@ -0,0 +1,49 @@
+//
+//  SendMailView.swift
+//  SendMail
+//
+//  Created by Jan Hovland on 10/10/2020.
+//
+
+/// Dokumentasjon: https://stackoverflow.com/questions/56784722/swiftui-send-email
+
+import SwiftUI
+import MessageUI
+
+struct SendMailView: View {
+    
+    @State var isShowingMailView = false
+    @State var toRecipients: [String] = []
+    @State var subject: String = ""
+    @State var name: String = ""
+    @State var messageBody: String = ""
+    @State var result: Result<MFMailComposeResult, Error>? = nil
+
+    var body: some View {
+        
+        VStack {
+            if MFMailComposeViewController.canSendMail() {
+                Button("Show mail view") {
+                    toRecipients = ["jan.hovland@lyse.net"]
+                    subject = "Fødselsdag hilsen"
+                    name = "Olufine"
+                    messageBody = "Gratulerer så mye med fødselsdagen din, " + name + ",🇳🇴 😀"
+                    self.isShowingMailView.toggle()
+                }
+            } else {
+                Text("Can't send emails from this device")
+            }
+        }
+        .sheet(isPresented: $isShowingMailView) {
+            MailView(isShowing: self.$isShowingMailView,
+                     toRecipients: self.$toRecipients,
+                     subject: self.$subject,
+                     messageBody: self.$messageBody,
+                     result: self.$result)
+                     
+        }
+        
+    }
+    
+}
+
