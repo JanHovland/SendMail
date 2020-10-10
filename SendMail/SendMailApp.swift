a593d14c509710d0ceb31ae672c31220207fa05c
Jan Hovland <jan.hovland@lyse.net>
2020-10-10 16:48:30 +0200

    Initial Commit

---
+++ SendMail/SendMailApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendMailApp.swift
+//  SendMail
+//
+//  Created by Jan Hovland on 10/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct SendMailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
